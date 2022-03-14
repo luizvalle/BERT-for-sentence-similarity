591acfd5d33e37a20ed79d711e5f29609aaa7bd0
Luiz <lld2131@columbia.edu>
2022-03-14 18:56:00 +0000

    Implemented cosine similarity

--- similarity.py
+++ similarity.py
@@ -1,15 +1,32 @@
 import sys
+import numpy as np
 
-def read_pairs():
+from sentence_transformers import SentenceTransformer
+from scipy.spatial.distance import cosine
+
+def read_pairs() -> tuple[list[str],list[str]]:
     first, second = list(), list()
     for line in sys.stdin:
-        f, s = line.split("[SEP]")
-        first.append(f.strip())
-        second.append(s.strip())
+        line = line.strip()
+        try:
+            f, s = line.split("[SEP]")
+            first.append(f)
+            second.append(s)
+        except ValueError:
+            print(f"Warning: Malformed line '{line}' skipped...", file=sys.stderr)
     return first, second
+    
+def encode_sentences(model: SentenceTransformer, sentences:list[str]) -> np.ndarray:
+    return model.encode(sentences)
 
 def main():
     first, second = read_pairs()
+    model = SentenceTransformer("bert-base-nli-mean-tokens")
+    first_enc = encode_sentences(model, first)
+    second_enc = encode_sentences(model, second)
+    for f_enc, s_enc in zip(first_enc, second_enc):
+        print(cosine(f_enc, s_enc))
+
 
 if __name__ == "__main__":
     main()
